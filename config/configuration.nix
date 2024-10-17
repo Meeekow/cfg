f9b7de4e5916af91e8a97529564a0c7bf141eac2
Meeekow <68207769+Meeekow@users.noreply.github.com>
2024-10-17 09:17:25 +0800

    bump config to match 24.05

--- config/configuration.nix
+++ config/configuration.nix
@@ -1,8 +1,8 @@
 # Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running `nixos-help`).
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   imports =
@@ -21,7 +21,6 @@
 
   # Set your time zone.
   time.timeZone = "Asia/Manila";
-  networking.timeServers = [ "time.google.com" ];
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
@@ -32,44 +31,49 @@
   # console = {
   #   font = "Lat2-Terminus16";
   #   keyMap = "us";
-  #   useXkbConfig = true; # use xkbOptions in tty.
+  #   useXkbConfig = true; # use xkb.options in tty.
   # };
 
   # Configure keymap in X11
-  services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e,caps:escape";
+  services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
 
   # Enable sound.
-  sound.enable = true;
-  # hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.enable = true;
+  # OR
+  # services.pipewire = {
+  #   enable = true;
+  #   pulse.enable = true;
+  # };
 
   # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  # services.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.meredith = {
-     isNormalUser = true;
-     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-     packages = with pkgs; [
-       firefox
-       gimp
-       gnome.dconf-editor
-       kitty
-       qbittorrent
-       vlc
-       xclip
-     ];
-     initialPassword = "pass";
+  users.users.rara = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+      chromium
+      firefox
+      kitty
+      mpv
+      protonvpn-gui
+      qbittorrent
+      xclip
+    ];
+    initialPassword = "pass";
   };
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-     git
-     wget
+    git
+    vim
+    wget
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -96,13 +100,24 @@
   # accidentally delete configuration.nix.
   # system.copySystemConfiguration = true;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.05"; # Did you read the comment?
 
   # Boot Options
   boot.loader.timeout = 10;
@@ -111,20 +126,10 @@
   # Chipset
   hardware.cpu.amd.updateMicrocode = true;
 
-  # Networking
-  networking.networkmanager.insertNameservers = [ "8.8.8.8" "8.8.4.4" ];
-  networking.nameservers = [ "8.8.8.8" "8.8.4.4" ];
-
   # Enable mounting of NTFS
   boot.supportedFilesystems = [ "ntfs" ];
 
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-  # Enable Gnome DE
-  # services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
-
+  # Enable the x11 windowing system, Gnome DE and Nvidia
   services.xserver = {
     enable = true;
     videoDrivers = [ "nvidia" ];
@@ -192,15 +197,5 @@
     };
   };
 
-  # Bash
-  programs.bash.shellAliases = {
-    l = "ls -ahl --color=tty";
-    v = "nvim";
-    nv = "sudo nvim";
-    cls = "history -c && history -w";
-    mkdir = "mkdir -p";
-
-    gs = "git status";
-  };
 }
 
